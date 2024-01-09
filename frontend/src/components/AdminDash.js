178ad870c9e126825d0010cceba948d6fd54bc4e
Gerardo <gerardoh13@gmail.com>
2024-01-08 23:16:10 -0800

    added AdminDash component

--- frontend/src/components/AdminDash.js
+++ frontend/src/components/AdminDash.js
@@ -2,55 +2,87 @@ import React, { useContext, useState, useEffect } from "react";
 import ProviderContext from "../common/ProviderContext";
 import SmartnosisApi from "../api";
 
-function AdminDash({ }) {
-  // const [intakes, setIntakes] = useState([]);
+function AdminDash() {
+  const [role, setRole] = useState("hcp");
+  const [invitations, SetInvitations] = useState({
+    hcps: { sent: [], active: [] },
+    staff: { sent: [], active: [] },
+  });
+
   const { currUser } = useContext(ProviderContext);
 
   useEffect(() => {
     const fetchAdminData = async () => {
-      let invitations = await SmartnosisApi.getInvitations(currUser.providerId)
-      console.log(invitations)
-        };
-        fetchAdminData();
+      let invites = await SmartnosisApi.getInvitations(currUser.providerId);
+      console.log(invites);
+      SetInvitations(invites);
+    };
+    fetchAdminData();
   }, [currUser.providerId]);
 
-  // const createRows = (arr) => {
-  //   return arr.map((p) => (
-  //     <tr key={p.id} className="align-middle">
-  //       <td>{`${p.lastName}, ${p.middleName ? p.middleName[0] + "." : ""} ${
-  //         p.firstName
-  //       }`}</td>
-  //       <td>{p.dob}</td>
-  //       <td>{formatTime(p.submittedAt)}</td>
-  //       <td>
-  //         <button className="btn btn-success" onClick={() => generatePdf(p.id)}>
-  //           PDF
-  //         </button>
-  //       </td>
-  //     </tr>
-  //   ));
-  // };
+  const createRows = (arr) => {
+    return arr.map((email) => (
+      <tr key={email} className="align-middle">
+        <td>{email}</td>
+        <td>
+          <button
+            className="btn btn-success"
+            onClick={() => console.log("click")}
+          >
+            PDF
+          </button>
+        </td>
+      </tr>
+    ));
+  };
 
   return (
     <div className="card">
       <div className="card-body">
         <h4 className="card-title ms-2 mb-3">Admin Dashboard</h4>
+        <div className="mt-3">
+          {/* <span className="text-start ms-1 me-3">
+        </span> */}
+          <div className="text-center">
+            {/* Hcp */}
+            <input
+              type="radio"
+              className="btn-check ms-3"
+              name="role"
+              id="hcp"
+              autoComplete="off"
+              onChange={() => setRole("hcp")}
+              checked={role === "hcp"}
+            />
+            <label className="btn btn-outline-secondary me-2" htmlFor="hcp">
+              HCP
+            </label>
+            {/* Non-HCP */}
+            <input
+              type="radio"
+              className="btn-check"
+              name="role"
+              id="staff"
+              autoComplete="off"
+              onChange={() => setRole("staff")}
+              checked={role === "staff"}
+            />
+            <label className="btn btn-outline-secondary me-2" htmlFor="staff">
+              Non-HCP
+            </label>
+          </div>
+        </div>
         <hr />
-        {/* {intakes.length ? (
-          <table className="table table-striped table-sm bg-light">
-            <thead>
-              <tr>
-                <th scope="col">Name</th>
-                <th scope="col">Date of Birth</th>
-                <th scope="col">Submitted</th>
-                <th />
-              </tr>
-            </thead>
-            <tbody>{createRows(intakes)}</tbody>
-          </table>
-        ) : (
-          <p className="text-center">Completed intakes will appear here</p>
-        )} */}
+
+        <table className="table table-striped table-sm bg-light">
+          <thead>
+            <tr>
+              <th scope="col">Email</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>{createRows(invitations.hcps.sent)}</tbody>
+        </table>
       </div>
     </div>
   );
